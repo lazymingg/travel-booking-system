08dc366b9841dca3580aa9169e475952371e8511
AnhTtis <nguyenhuuanhtri866@gmail.com>
2025-08-19 01:23:30 +0700

    Finish About Us

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -23,6 +23,11 @@ const router = createRouter({
       path: '/manage-reservations',
       name: 'ManageReservations',
       component: () => import('../views/ManageReservations.vue'),
+    },
+    {
+      path: '/about-us',
+      name: 'AboutUs',
+      component: () => import('../views/AboutUs.vue'),
     }
   ],
 })
