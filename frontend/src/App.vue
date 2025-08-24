04a65f63c38e1f7bcb097d5736e6a54fe9fda916
Tran Cao Van <trancaovan0701@gmail.com>
2025-08-24 22:48:30 +0700

    Upload accommodation finishe

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -25,7 +25,6 @@ const { showError, errorMessage, errorDetails, clearError } = useError()
 <style scoped>
 .app {
   min-height: 100vh;
-  display: flex;
   flex-direction: column;
 }
 router-view {
